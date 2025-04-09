33f2481dcea2f54915142accb2609b8f6f54dc0f
viktorprogram <sergowork00@mail.ru>
2025-04-09 16:53:45 +0300

    изменение пути к фото

--- GamebitBot/utils/utils_bot.py
+++ GamebitBot/utils/utils_bot.py
@@ -18,8 +18,8 @@ def info_pk_user(choosing_place: str) -> str:
 
 def open_photo_to_message(name: str):
     """Функция чтения изображения для отправки"""
-    open_photo = [open((f'{os.getcwd()}\\GamebitBot\\photo\\{name}\\{path}'), 'rb') for path in
-                          os.listdir(f'{os.getcwd()}\\GamebitBot\\photo\\{name}')]
+    open_photo = [open((f'{os.getcwd()}/GamebitBot/photo/{name}/{path}'), 'rb') for path in
+                          os.listdir(f'{os.getcwd()}/GamebitBot/photo/{name}')]
     media_photo = [InputMediaPhoto(i) for i in open_photo]
     return open_photo, media_photo
 
